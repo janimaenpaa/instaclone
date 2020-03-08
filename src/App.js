2b7814f6b6aac578e74a23242ae11c12778e810d
janimaenpaa <itsmayd@gmail.com>
2020-03-08 18:17:46 +0200

    Add user token

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import jwtDecode from "jwt-decode";
 
 import "./App.css";
 import "typeface-roboto";
 
-import { createMuiTheme, ThemeProvider } from "@material-ui/core/styles";
-
 import NavBar from "./components/NavBar";
+import AuthRoute from "./util/AuthRoute";
 
 import Home from "./pages/Home";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 
+let authenticated;
+const token = localStorage.fbIdToken;
+
+if (token) {
+  const decodedToken = jwtDecode(token);
+  if (decodedToken.exp * 1000 < Date.now()) {
+    window.location.href = "/login";
+    authenticated = false;
+  } else {
+    authenticated = true;
+  }
+}
 
 const App = () => {
   return (
     <div className="App">
       <Router>
         <div className="nav-container">
-        <NavBar />
+          <NavBar />
         </div>
         <div className="container">
           <Switch>
             <Route exact path="/" component={Home} />
-            <Route path="/login" component={Login} />
-            <Route path="/signup" component={Signup} />
+            <AuthRoute
+              path="/login"
+              component={Login}
+              authenticated={authenticated}
+            />
+            <AuthRoute
+              path="/signup"
+              component={Signup}
+              authenticated={authenticated}
+            />
           </Switch>
         </div>
       </Router>
--- src/components/Post.js
+++ src/components/Post.js
@@ -16,7 +16,7 @@ import { makeStyles } from "@material-ui/core/styles";
 const useStyles = makeStyles(theme => ({
   card: {
     margin: theme.spacing(2),
-    width: "100%",
+    maxWidth: "100%",
     border: "1px solid",
     borderColor: "#dbdbdb",
     boxShadow: "none"
